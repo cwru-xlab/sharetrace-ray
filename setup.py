9aeb350201b68529d00f748c861ef6106e72d4db
rtatton <rdt17@protonmail.com>
2021-10-02 19:59:02 -0400

    Add requirements, setup; update .gitignore
    Signed-off-by: rtatton <rdt17@protonmail.com>

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+	name='ray-graph',
+	version='0.1.0',
+	packages=[''],
+	url='https://github.com/csds438-f21-project/ray-graph',
+	license='MIT',
+	author='Ryan Tatton',
+	author_email='ryan.tatton@case.edu',
+	description='Graph processing on Ray '
+)
