66ccf916cc679f74aeedcfaaa2d4f8595c2a1232
rtatton <rdt17@protonmail.com>
2021-11-17 15:24:16 -0500

    New data generation

--- evaluation/synthetic.py
+++ evaluation/synthetic.py
@@ -1,37 +1,92 @@
 import datetime
-import logging.config
 import pathlib
 from typing import Callable, Tuple
 
 import joblib
 import numpy as np
+from numpy import arange, clip, concatenate, datetime64, vstack, zeros
 from scipy import stats
 
-import sharetrace.util
 from sharetrace import model, propagation, search
-
-logging.config.dictConfig(sharetrace.util.LOGGING_CONFIG)
-logger = logging.getLogger(__name__)
 
 NOW = np.datetime64(datetime.datetime.utcnow(), 's')
 rng = np.random.default_rng()
-DAY_IN_SECONDS = 86_400
+SEC_PER_DAY = 86400
+MIN_PER_HR = 3600
 HISTORIES_FILENAME_FORMAT = 'histories_%s'
 HISTORIES_FILE_FORMAT = 'histories_%s.npy'
 SCORES_FILENAME_FORMAT = 'scores_%s'
 SCORES_FILE_FORMAT = 'scores_%s.npy'
 
+LATS = (-90, 90)
+LONGS = (-180, 180)
+
+
+def walk(
+        n: int,
+        low: float = -1,
+        high: float = 1,
+        step_low: float = -0.1,
+        step_high: float = 0.1,
+        xinit: float = 0,
+        yinit: float = 0):
+    def step(pos, idx, dim):
+        delta = rng.uniform(step_low, step_high)
+        pos[dim][idx] = clip(pos[dim][idx - 1] + delta, low, high)
+
+    xy = zeros((2, n))
+    xy[0][0] = clip(xinit, low, high)
+    xy[1][0] = clip(yinit, low, high)
+    for i in range(1, n):
+        step(xy, i, 0)
+        step(xy, i, 1)
+    return xy
+
+
+def create_locs(n: int, steps: int, dist, **kwargs):
+    locs = zeros((n, 2, steps))
+    for i in range(n):
+        xinit, yinit = dist.rvs(size=2)
+        locs[i, ::] = walk(steps, xinit=xinit, yinit=yinit, **kwargs)
+    return locs
+
+
+def create_times(n: int, now: datetime64, days: int = 14, per_day: int = 16):
+    dt_type, td_type = 'datetime64[s]', 'timedelta64[s]'
+    now = datetime64(now).astype(dt_type)
+    days += 1
+    times = zeros((n, days * per_day), dtype=dt_type)
+    offsets = (arange(days) * SEC_PER_DAY).astype(td_type)
+    starts = now - offsets
+    for i in range(n):
+        delta = rng.uniform(0, MIN_PER_HR, size=per_day).cumsum()
+        delta = delta.astype(td_type)
+        times[i, :] = concatenate([start + delta for start in starts])
+    return times
+
+
+def take(a, idx, axis):
+    cut = [slice(None)] * a.ndim
+    cut[axis] = idx
+    return a[tuple(cut)]
+
+
+def create_scores(locs, dist, z, days_back: int = 14, per_day: int = 16):
+    idx = arange((days_back + 1) * per_day, step=per_day)
+    selected = take(locs, idx, -1)
+    return vstack([dist.pdf(x.T) / z for x in selected])
+
 
 def random_time(n: int = 1, lookback: int = 14):
-    lookback *= DAY_IN_SECONDS
+    lookback *= SEC_PER_DAY
     return NOW - rng.integers(0, lookback, size=n).astype('timedelta64[s]')
 
 
 def random_coordinate(
         n: int = 1,
         lookback: int = 14,
-        lats: Tuple = (-90, 90),
-        longs: Tuple = (-180, 180)):
+        lats: Tuple = LATS,
+        longs: Tuple = LONGS):
     lats = rng.uniform(*lats, size=n)
     longs = rng.uniform(*longs, size=n)
     times = random_time(n, lookback)
@@ -43,8 +98,8 @@ def random_coordinate(
 def random_geohash(
         n: int = 1,
         lookback: int = 14,
-        lats: Tuple = (-90, 90),
-        longs: Tuple = (-180, 180),
+        lats: Tuple = LATS,
+        longs: Tuple = LONGS,
         precision: int = 12):
     locs = random_coordinate(n, lookback, lats, longs)
     return np.array([model.to_geohash(loc, precision) for loc in locs])
@@ -55,8 +110,8 @@ def _random_locs(
         events: int = 10,
         lookback: int = 14,
         precision: int = 12,
-        lats: Tuple = (-90, 90),
-        longs: Tuple = (-180, 180),
+        lats: Tuple = LATS,
+        longs: Tuple = LONGS,
         geohash: bool = True):
     if geohash:
         locs = random_geohash(events, lookback, lats, longs, precision)
@@ -70,11 +125,10 @@ def random_history(
         events: int = 10,
         lookback: int = 14,
         precision: int = 12,
-        lats: Tuple = (-90, 90),
-        longs: Tuple = (-180, 180),
+        lats: Tuple = LATS,
+        longs: Tuple = LONGS,
         geohash: bool = True,
         workers: int = 1):
-    logger.debug('Generating %d location histories', n)
     par = joblib.Parallel(workers)
     kwargs = {
         'events': events,
@@ -92,7 +146,6 @@ def random_score(
         lookback: int = 14,
         alpha: float = 0.5,
         beta: float = 0.5):
-    logger.debug('Generating %d scores', n)
     risk_scores = []
     for _ in range(n):
         values = stats.beta.rvs(alpha, beta, size=scores)
@@ -107,8 +160,8 @@ def generate_histories(
         events: int = 10,
         lookback: int = 14,
         precision: int = 8,
-        lats: Tuple = (-90, 90),
-        longs: Tuple = (-180, 180),
+        lats: Tuple = LATS,
+        longs: Tuple = LONGS,
         geohash: bool = True,
         workers: int = 1):
     return generate(
@@ -145,15 +198,14 @@ def generate(func: Callable, file: str, filename: str):
 
 
 def load_histories(n: int):
-    return load(n, 'histories', HISTORIES_FILE_FORMAT % n)
+    return load(HISTORIES_FILE_FORMAT % n)
 
 
 def load_scores(n: int):
-    return load(n, 'scores', SCORES_FILE_FORMAT % n)
+    return load(SCORES_FILE_FORMAT % n)
 
 
-def load(n: int, name: str, file: str):
-    logger.debug('Loading %d %s', n, name)
+def load(file: str):
     return np.load(file, allow_pickle=True)
 
 
@@ -187,7 +239,7 @@ def main():
     scores = generate_scores(n)
     contact_search = search.KdTreeContactSearch(min_dur=900, workers=-1)
     contacts = contact_search.search(histories)
-    for t in np.arange(0.1, 1.1, 0.1):
+    for t in np.arange(0.1, 1, 0.1):
         risk_propagation = propagation.RayRiskPropagation(
             parts=4, early_stop=10_000, tol=t, timeout=3)
         risk_propagation.setup(scores, contacts)
