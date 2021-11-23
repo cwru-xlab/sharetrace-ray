2849353a84310866069c3300daf0302a2c53ee9a
rtatton <rdt17@protonmail.com>
2021-11-22 22:55:48 -0500

    New contact search
    Uses a combination of interpolation and Euclidean distance thresholding to determine if users came into contact. Increasing the joblib batch size and reducing the time resolution to minutes were essential to maintaining good performance.

--- sharetrace/search.py
+++ sharetrace/search.py
@@ -1,246 +1,187 @@
-import itertools
+import datetime
 import json
 import logging
-from abc import ABC, abstractmethod
-from enum import Enum
-from typing import Any, Dict, Iterable, Optional, Sequence, Tuple
+from typing import NoReturn, Optional, Sequence, Tuple
 
 import joblib
 import numpy as np
 import pyproj
-from scipy import spatial
+from scipy import interpolate, spatial
 
 from sharetrace import model, util
 
 Locations = Histories = Sequence[np.void]
 Contacts = np.ndarray
-Pairs = Iterable[Tuple[np.void, np.void]]
-
-rng = np.random.default_rng()
-EMPTY = ()
 
 
-class Kind(Enum):
-    BRUTE = 'brute'
-    KD_TREE = 'kd_tree'
-
-
-class BaseContactSearch(ABC):
-    __slots__ = ('logger', 'min_dur', 'workers')
+class ContactSearch:
+    __slots__ = (
+        'min_dur',
+        'r',
+        'leaf_size',
+        'p',
+        'eps',
+        'tol',
+        'workers',
+        'logger')
 
     def __init__(
             self,
             min_dur: float = 0,
+            r: float = 1e-4,
+            leaf_size: int = 10,
+            p: float = 2,
+            eps: float = 1e-5,
+            tol: float = 0.01,
             workers: int = 1,
             logger: Optional[logging.Logger] = None):
-        super().__init__()
         self.min_dur = min_dur
+        self.r = r
+        self.leaf_size = leaf_size
+        self.p = p
+        self.eps = eps
+        self.tol = tol
         self.workers = workers
         self.logger = logger
-
-    @abstractmethod
-    def search(self, histories: Histories) -> Contacts:
-        pass
-
-    @abstractmethod
-    def pairs(self, histories: Histories) -> Pairs:
-        pass
-
-
-class BruteContactSearch(BaseContactSearch):
-    __slots__ = ('kind', '_min_dur')
-
-    def __init__(
-            self,
-            min_dur: float = 0,
-            workers: int = 1,
-            logger: Optional[logging.Logger] = None):
-        super().__init__(min_dur, workers, logger)
-        self.kind = Kind.BRUTE
-        self._min_dur = np.timedelta64(int(min_dur * 1e6), 'us')
 
     def search(self, histories: Histories) -> Contacts:
         timer = util.time(lambda: self._search(histories))
         result = timer.result
-        stats = self.stats(len(histories), len(result), timer.seconds)
-        if self.logger is not None:
-            util.info(self.logger, json.dumps(stats))
+        self._log_stats(len(histories), len(result), timer.seconds)
         return result
 
-    def stats(self, n: int, contacts: int, runtime: float) -> Dict[str, Any]:
-        return {
-            'Kind': self.kind.name,
-            'RuntimeInSec': util.approx(runtime),
-            'Workers': self.workers,
-            'MinDurationInSec': self.min_dur,
-            'InputSize': n,
-            'Contacts': contacts}
-
     def _search(self, histories: Histories) -> Contacts:
-        pairs = self.pairs(histories)
-        par = joblib.Parallel(self.workers, verbose=2)
-        contacts = par(joblib.delayed(self._find_contact)(*p) for p in pairs)
-        contacts = np.array([c for c in contacts if c is not None])
-        return contacts
+        pairs, locs = self._selected(histories)
+        print(len(pairs))
+        par = joblib.Parallel(self.workers, batch_size=500)
+        find_contact = joblib.delayed(self._find_contact)
+        contacts = par(find_contact(p, histories, locs) for p in pairs)
+        return np.array([c for c in contacts if c is not None])
 
-    def pairs(self, histories: Histories) -> Pairs:
-        return itertools.combinations(histories, 2)
-
-    def _find_contact(self, h1: np.void, h2: np.void) -> Optional[np.void]:
-        found = None
-        name1, name2 = h1['name'], h2['name']
-        if name1 != name2:
-            events = self._find(h1['locs'], 0, h2['locs'], 0)
-            if len(events) > 0:
-                found = model.contact((name1, name2), events)
-        return found
-
-    def _find(
+    def _find_contact(
             self,
-            locs1: np.ndarray,
-            i1: int,
-            locs2: np.ndarray,
-            i2: int
-    ) -> Sequence[np.void]:
-        events = []
-        later, create_event, find, add_events = (
-            self._later, self._event, self._find, events.extend)
-        len1, len2 = len(locs1) - 1, len(locs2) - 1
-        loc1, loc2 = locs1[i1], locs2[i2]
-        started = False
-        while i1 < len1 and i2 < len2:
-            if loc1['loc'] == loc2['loc']:
-                if started:
-                    i1, i2 = i1 + 1, i2 + 1
-                    loc1, loc2 = locs1[i1], locs2[i2]
-                else:
-                    started = True
-                    start = later(loc1, loc2)
-            elif started:
-                started = False
-                # noinspection PyUnboundLocalVariable
-                add_events(create_event(start, loc1, loc2))
-            elif loc1['time'] < loc2['time']:
-                i1 += 1
-                loc1 = locs1[i1]
-            elif loc2['time'] < loc1['time']:
-                i2 += 1
-                loc2 = locs2[i2]
-            else:
-                add_events(find(locs1, i1 + 1, locs2, i2))
-                add_events(find(locs1, i1, locs2, i2 + 1))
-                break
-        if started:
-            add_events(create_event(start, loc1, loc2))
-        return events
+            pair: np.ndarray,
+            histories: Histories,
+            locs: np.ndarray
+    ) -> Optional[np.void]:
+        u1, u2 = pair
+        hist1, hist2 = histories[u1], histories[u2]
+        times1, locs1 = _resample(hist1['locs']['time'], locs[u1])
+        times2, locs2 = _resample(hist2['locs']['time'], locs[u2])
+        times1, locs1, times2, locs2 = _pad(times1, locs1, times2, locs2)
+        close = self._proximal(locs1, locs2)
+        ints = _intervals(close)
+        contact = None
+        if len(ints) > 0:
+            options = np.flatnonzero(
+                ints[:, 1] - ints[:, 0] >= self.min_dur / 60)
+            if len(options) > 0:
+                names = (hist1['name'], hist2['name'])
+                most_recent = ints[options[-1]]
+                start, end = most_recent
+                duration = np.timedelta64(end - start, 'm')
+                time = datetime.datetime.utcfromtimestamp(times1[start])
+                contact = model.contact(names, time, duration)
+        return contact
 
-    def _event(self, start, loc1: np.void, loc2: np.void) -> Iterable[np.void]:
-        end = self._earlier(loc1, loc2)
-        dur = end - start
-        return [model.event(start, dur)] if dur >= self._min_dur else EMPTY
+    def _selected(self, histories: Histories) -> Tuple:
+        locs = self._coordinates(histories)
+        idx = _index(locs)
+        pairs = self._query_pairs(np.concatenate(locs))
+        return _select(pairs, idx), locs
 
-    @staticmethod
-    def _later(loc1: np.void, loc2: np.void) -> float:
-        t1, t2 = loc1['time'], loc2['time']
-        return t1 if t1 > t2 else t2
-
-    @staticmethod
-    def _earlier(loc1: np.void, loc2: np.void) -> float:
-        t1, t2 = loc1['time'], loc2['time']
-        return t1 if t1 < t2 else t2
-
-
-class TreeContactSearch(BruteContactSearch):
-    __slots__ = ('r', 'leaf_size')
-
-    def __init__(
-            self,
-            min_dur: float = 0,
-            workers: int = 1,
-            r: float = 1e-4,
-            leaf_size: int = 10,
-            logger: Optional[logging.Logger] = None):
-        super().__init__(min_dur, workers, logger)
-        self.r = r
-        self.leaf_size = leaf_size
-
-    def pairs(self, histories: Histories) -> Pairs:
-        locs = self.to_coordinates(histories)
-        idx = self.index(locs)
-        pairs = self.query_pairs(np.concatenate(locs))
-        return self.filter(pairs, idx, histories)
-
-    def to_coordinates(self, hists: Histories) -> Histories:
+    def _coordinates(self, hists: Histories) -> Histories:
         par = joblib.Parallel(self.workers)
-        return par(joblib.delayed(self._to_coordinates)(h) for h in hists)
+        return par(joblib.delayed(_coords)(h) for h in hists)
 
-    def stats(self, n: int, contacts: int, runtime: float) -> Dict[str, Any]:
-        stats = super().stats(n, contacts, runtime)
-        stats.update({'Radius': self.r, 'LeafSize': self.leaf_size})
-        return stats
-
-    @staticmethod
-    def index(locations: Locations) -> np.ndarray:
-        idx = np.arange(len(locations))
-        repeats = [len(locs) for locs in locations]
-        return np.repeat(idx, repeats)
-
-    def query_pairs(self, locs: np.ndarray) -> np.ndarray:
-        raise NotImplementedError
-
-    @staticmethod
-    def filter(pairs: np.ndarray, idx: np.ndarray, hists: Histories) -> Pairs:
-        selected = np.unique(idx[pairs], axis=0)
-        return ((hists[h1], hists[h2]) for (h1, h2) in selected if h1 != h2)
-
-    @staticmethod
-    def _to_coordinates(hist: np.ndarray) -> np.ndarray:
-        return np.vstack([model.to_coord(loc)['loc'] for loc in hist['locs']])
-
-
-class KdTreeContactSearch(TreeContactSearch):
-    __slots__ = ('p', 'eps')
-
-    def __init__(
-            self,
-            min_dur: float = 0,
-            workers: int = 1,
-            r: float = 1e-4,
-            leaf_size: int = 10,
-            eps: int = 1e-5,
-            p: float = 2,
-            logger: Optional[logging.Logger] = None):
-        super().__init__(min_dur, workers, r, leaf_size, logger)
-        self.kind = Kind.KD_TREE
-        self.eps = eps
-        self.p = p
-
-    def query_pairs(self, locs: np.ndarray) -> np.ndarray:
-        locs = self._project(locs)
+    def _query_pairs(self, locs: np.ndarray) -> np.ndarray:
+        locs = _project(locs)
         kd_tree = spatial.KDTree(locs, self.leaf_size)
         return kd_tree.query_pairs(self.r, self.p, self.eps, 'ndarray')
 
-    def stats(self, n: int, contacts: int, runtime: float) -> Dict[str, Any]:
-        stats = super().stats(n, contacts, runtime)
-        stats.update({'Epsilon': self.eps, 'MinkowskiNorm': self.p})
-        return stats
+    def _proximal(self, locs1: np.ndarray, locs2: np.ndarray) -> np.ndarray:
+        diff = np.linalg.norm(locs1.T - locs2.T, axis=1)
+        return np.flatnonzero(diff <= self.tol)
 
-    @staticmethod
-    def _project(coordinates: np.ndarray) -> np.ndarray:
-        """Project from latitude-longitude to x-y Cartesian coordinates.
+    def _log_stats(self, n: int, contacts: int, runtime: float) -> NoReturn:
+        if self.logger is not None:
+            util.info(self.logger, json.dumps({
+                'RuntimeInSec': util.approx(runtime),
+                'Workers': self.workers,
+                'MinDurationInSec': self.min_dur,
+                'InputSize': n,
+                'Contacts': contacts,
+                'Radius': self.r,
+                'LeafSize': self.leaf_size,
+                'MinkowskiNorm': self.p,
+                'Epsilon': self.eps,
+                'Tolerance': self.tol}))
 
-            References:
-                https://stackoverflow.com/a/54039559
 
-            Args:
-                coordinates: A (N, 2) numpy array of latitude-longitude pairs.
+def _coords(hist: np.void) -> np.ndarray:
+    return model.to_coords(hist)['locs']['loc']
 
-            Returns:
-                A (N, 2) numpy array of x-y Cartesian pairs.
-        """
-        lats, longs = coordinates.T
-        ecef = pyproj.Proj(proj='geocent', ellps='WGS84', datum='WGS84')
-        lla = pyproj.Proj(proj='latlong', ellps='WGS84', datum='WGS84')
-        transformer = pyproj.Transformer.from_proj(lla, ecef)
-        projected = transformer.transform(longs, lats, radians=False)
-        return np.column_stack(projected)
+
+def _index(locations: Locations) -> np.ndarray:
+    users = np.arange(len(locations))
+    repeats = [len(locs) for locs in locations]
+    return np.repeat(users, repeats)
+
+
+def _project(coordinates: np.ndarray) -> np.ndarray:
+    """Projects from latitude-longitude to x-y Cartesian coordinates."""
+    lats, longs = coordinates.T
+    ecef = pyproj.Proj(proj='geocent', ellps='WGS84', datum='WGS84')
+    lla = pyproj.Proj(proj='latlong', ellps='WGS84', datum='WGS84')
+    transformer = pyproj.Transformer.from_proj(lla, ecef)
+    projected = transformer.transform(longs, lats, radians=False)
+    return np.column_stack(projected)
+
+
+def _select(queried: np.ndarray, idx: np.ndarray) -> np.ndarray:
+    selected = np.unique(idx[queried], axis=0)
+    return selected[~(selected[:, 0] == selected[:, 1])]
+
+
+def _resample(times: np.ndarray, locs: np.ndarray) -> Tuple:
+    # Second resolution results in really slow performance.
+    times = np.int64(times.astype('datetime64[m]'))
+    # Prefer interp1d over np.interp to use 'previous' interpolation.
+    interp = interpolate.interp1d(
+        times, locs.T, kind='previous', assume_sorted=True)
+    new_times = np.arange(times[0], times[-1])
+    new_locs = interp(new_times)
+    return new_times, new_locs
+
+
+def _intervals(a: np.ndarray) -> np.ndarray:
+    split_at = np.flatnonzero(np.diff(a) != 1)
+    chunks = np.split(a, split_at + 1)
+    has_splits = not (len(chunks) == 1 and len(chunks[0]) == 0)
+    intervals = [(ch[0], ch[-1] + 1) for ch in chunks] if has_splits else []
+    return np.array(intervals, dtype=np.int64)
+
+
+def _pad(
+        times1: np.ndarray,
+        locs1: np.ndarray,
+        times2: np.ndarray,
+        locs2: np.ndarray
+) -> Tuple:
+    new_start = min(times1[0], times2[0])
+    new_end = max(times1[-1], times2[-1])
+    (new_times1, new_locs1) = _expand(times1, locs1, new_start, new_end)
+    (new_times2, new_locs2) = _expand(times2, locs2, new_start, new_end)
+    return new_times1, new_locs1, new_times2, new_locs2
+
+
+def _expand(
+        times: np.ndarray, locs: np.ndarray, new_start: int, new_end: int
+) -> Tuple:
+    prepend = np.arange(new_start, times[0])
+    append = np.arange(times[-1] + 1, new_end + 1)
+    new_times = np.concatenate((prepend, times, append))
+    prepend = np.ones((2, len(prepend))) * np.inf
+    append = np.ones((2, len(append))) * np.inf
+    new_locs = np.column_stack((prepend, locs, append))
+    return new_times, new_locs
