c305e20b061ae63ae232c4174d50694f85be2a34
rtatton <rdt17@protonmail.com>
2022-01-09 14:11:40 -0500

    Add statements to load python
    Signed-off-by: rtatton <rdt17@protonmail.com>

--- evaluation/scripts/setup.sh
+++ evaluation/scripts/setup.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
 PACKAGE="contact-tracing"
+PYTHON_VERSION="3.8.6"
+
+module swap intel gcc
+module load python/$PYTHON_VERSION
 
 export PYTHONUSERBASE=$HOME/.usr/local/python/$PYTHON_VERSION
 mkdir -p $PYTHONUSERBASE
 pip install --upgrade pip
-pip install -r ~/$PACKAGE/requirements.txt --user
-pip install -e ~/$PACKAGE
+pip install -r $HOME/$PACKAGE/requirements.txt --user
+pip install -e $HOME/$PACKAGE
