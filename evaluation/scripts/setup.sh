8e831623a58eb3529b3f3b84143caacbe0521507
rtatton <rdt17@protonmail.com>
2022-01-10 00:11:56 -0500

    "Revert" scripts
    Effectively revert scripts to how they were originally with the pip statements.
    
    Signed-off-by: rtatton <rdt17@protonmail.com>

--- evaluation/scripts/setup.sh
+++ evaluation/scripts/setup.sh
@@ -1,13 +1,6 @@
 #!/bin/sh
 
-PACKAGE="contact-tracing"
 PYTHON_VERSION="3.8.6"
-
-module swap intel gcc
-module load python/$PYTHON_VERSION
 
 export PYTHONUSERBASE=$HOME/.usr/local/python/$PYTHON_VERSION
 mkdir -p $PYTHONUSERBASE
-python3 -m pip install --upgrade pip
-python3 -m pip install -r $HOME/$PACKAGE/requirements.txt --user
-python3 -m pip install -e $HOME/$PACKAGE
