38d3bc203fa123a66ee9deacd9cf6a2a004935f8
Ryan Tatton <rdt17@protonmail.com>
2022-03-24 14:11:57 -0400

    Update setup.py URL

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setuptools.setup(
     version="0.1.0",
     packages=setuptools.find_packages(),
     install_requires=["pymetis @ git+https://git@github.com/inducer/pymetis"],
-    url="https://github.com/csds438-f21-project/contact-tracing",
+    url="https://github.com/share-trace/contact-tracing",
     license="MIT",
     author="Ryan Tatton",
     author_email="ryan.tatton@case.edu",
